f260ef576ea0f3ff4e7e581e10f09648e6afc3a9
Simbadev <maxime.gasnier.1@gmail.com>
2018-01-14 15:05:55 +0100

    create all controllers for first group views

---
+++ app/scripts/controllers/authors.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wriMobileApp.controller:AuthorsCtrl
+ * @description
+ * # AuthorsCtrl
+ * Controller of the wriMobileApp
+ */
+angular.module('wriMobileApp')
+  .controller('AuthorsCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
