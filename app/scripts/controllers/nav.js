6504546ccef060654364ae8f0eeaf5f363261777
Simbadev <maxime.gasnier.1@gmail.com>
2018-01-14 15:07:32 +0100

    make all

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name wriMobileApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the wriMobileApp
+ */
+angular.module('wriMobileApp')
+  .controller('NavCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/services/notes.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name wriMobileApp.notes
+ * @description
+ * # notes
+ * Service in the wriMobileApp.
+ */
+angular.module('wriMobileApp')
+  .service('notes', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
